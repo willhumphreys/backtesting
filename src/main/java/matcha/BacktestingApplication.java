c93300f32d2a41969d4c4571d1a2da0d199ff133
Will Humphreys <will.humphreys@bbc.co.uk>
2016-07-29 10:19:44 +0100

    Add initial spring files

---
+++ src/main/java/matcha/BacktestingApplication.java
@@ -0,0 +1,12 @@
+package matcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacktestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacktestingApplication.class, args);
+	}
+}
