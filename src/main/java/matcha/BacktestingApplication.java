4eff52aa38b640c13656481e2048e37f3b469723
Will Humphreys <will.humphreys@bbc.co.uk>
2016-07-29 10:29:25 +0100

    Implement the CommandLineRunner

--- src/main/java/matcha/BacktestingApplication.java
+++ src/main/java/matcha/BacktestingApplication.java
@@ -1,12 +1,18 @@
 package matcha;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BacktestingApplication {
+public class BacktestingApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BacktestingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hello there");
 	}
 }
