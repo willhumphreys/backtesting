b725a6ce74a5cb338bbfe1980614c9b604ec281e
Will Humphreys <will.humphreys@bbc.co.uk>
2016-08-01 13:49:48 +0100

    Add other date fields.

--- src/main/java/matcha/Simulation.java
+++ src/main/java/matcha/Simulation.java
@@ -4,9 +4,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Simulation {
+    private static final int DATE = 0;
+    private static final int OPEN = 1;
+    private static final int LOW = 2;
+    private static final int HIGH = 3;
+    private static final int CLOSE = 4;
+    private static final int DAILY_LOW = 5;
+    private static final int DAILY_HIGH = 6;
+
     Results execute(String[][] data) {
 
-        System.out.println(data[0][0]);
+        for (String[] line : data) {
+
+            System.out.println(line[DATE] + line[OPEN] + line[LOW] + line[HIGH] + line[CLOSE] + line[DAILY_LOW] + line[DAILY_HIGH]);
+
+        }
+
+
 
         return null;
     }
