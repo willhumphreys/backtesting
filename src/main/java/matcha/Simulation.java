e97114fd27ee4c14b83c248e74f71b5a39e2b7d6
Will Humphreys <will.humphreys@bbc.co.uk>
2016-08-01 14:24:25 +0100

    Add some initial calculations

--- src/main/java/matcha/Simulation.java
+++ src/main/java/matcha/Simulation.java
@@ -2,6 +2,8 @@ package matcha;
 
 import org.springframework.stereotype.Service;
 
+import static java.lang.Float.parseFloat;
+
 @Service
 public class Simulation {
     private static final int DATE = 0;
@@ -13,6 +15,44 @@ public class Simulation {
     private static final int DAILY_HIGH = 6;
 
     Results execute(String[][] data) {
+
+
+        for (int i = 0; i < data.length; i++) {
+
+
+            float CandleClose = parseFloat(data[i][DATE]);
+            float CandleOpen = parseFloat(data[i][OPEN]);
+            float CandleLow = parseFloat(data[i][LOW];
+            float PreviousCandleLow = parseFloat(data[i -1][LOW]);
+            float CandleHigh = parseFloat(data[i][HIGH]);
+            float PreviousCandleHigh = parseFloat(data[i -1][HIGH]);
+            float PreviousCandleClose = parseFloat(data[i-1][CLOSE]);
+
+            float yesterdaysLow = parseFloat(data[i][DAILY_LOW]);
+            float yesterdaysHigh = parseFloat(data[i][DAILY_HIGH]);
+
+            float LastBarSize = PreviousCandleClose - PreviousCandleLow;
+
+
+            Boolean takeOutYesterdaysLow = CandleLow < yesterdaysLow;
+            Boolean closePositive = CandleClose > CandleOpen;
+            Boolean closeAboveYesterdaysLow = CandleClose > yesterdaysLow;
+            Boolean openAboveYesterdaysLow = CandleOpen > yesterdaysLow;
+
+            Boolean takeOutYesterdaysHigh = CandleHigh > yesterdaysHigh;
+            Boolean closeNegative = CandleClose < CandleOpen;
+            Boolean closeBelowYesterdaysHigh = CandleClose < yesterdaysHigh;
+            Boolean openBelowYesterdaysLow = CandleOpen < yesterdaysHigh;
+
+
+//            if (takeOutYesterdaysLow &&
+//                    closePositive &&
+//                    closeAboveYesterdaysLow &&
+//                    openAboveYesterdaysLow &&
+//                    getLowCheck(sc, lowOfTheDay, CandleLow, PreviousCandleLow, highLowCheckPrefInput.GetIndex()) &&
+//                    sc.GetBarHasClosedStatus() == BHCS_BAR_HAS_CLOSED) {
+
+        }
 
         for (String[] line : data) {
             System.out.println(line[DATE] + line[OPEN] + line[LOW] + line[HIGH] + line[CLOSE] + line[DAILY_LOW] + line[DAILY_HIGH]);
