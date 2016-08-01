8ba4f0647d614c467114278008fa965120bf709f
Will Humphreys <will.humphreys@bbc.co.uk>
2016-08-01 13:21:48 +0100

    Add code to load csv file. Add build directories to git ignore.

--- src/main/java/matcha/BacktestingApplication.java
+++ src/main/java/matcha/BacktestingApplication.java
@@ -1,18 +1,33 @@
 package matcha;
 
+import com.opencsv.CSVReader;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+
 @SpringBootApplication
-public class BacktestingApplication implements CommandLineRunner{
+public class BacktestingApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BacktestingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BacktestingApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("hello there");
-	}
+    @Override
+    public void run(String... args) throws Exception {
+
+        CSVReader csvReader = new CSVReader(new FileReader(new File("Data/dataOut2.txt")));
+        List<String[]> list = csvReader.readAll();
+
+        list.forEach(line -> System.out.println(line[0]));
+
+        String[][] dataArr = new String[list.size()][];
+        dataArr = list.toArray(dataArr);
+
+
+
+    }
 }
