e23f4843a2e57f762444308b7ee30a8831156a25
Will Humphreys <whumphreys@gmail.com>
2016-08-07 17:14:17 +0100

    Tidyup

--- RScripts/cumulativeProfit.r
+++ RScripts/cumulativeProfit.r
@@ -4,10 +4,14 @@ generate.plot <- function(file.in) {
     file.out <- paste("graphs/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], ".png", sep = "")
 
     data <- read.table(paste("results/",file.in, sep=""), header=T,sep=",")
-    data$date.time=as.POSIXct(data$date, tz = "UTC", format="%Y-%m-%dT%H:%M:%S")
+    data$date.time=as.Date(as.POSIXct(data$date, tz = "UTC", format="%Y-%m-%dT%H:%M:%S"))
     data$date <- NULL
 
-    ggplot(data=data, aes(x=date.time, y=cumulative_profit, group = 1)) + geom_line()
+    ggplot(data=data, aes(x=date.time, y=cumulative_profit, group = 1)) +
+    geom_line() +
+    scale_x_date(date_breaks = "3 month") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    ggtitle(file.in)
     ggsave(file=file.out)
 }
 in_files <- list.files('results')
--- src/main/java/matcha/BacktestingApplication.java
+++ src/main/java/matcha/BacktestingApplication.java
@@ -10,8 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class BacktestingApplication implements CommandLineRunner {
