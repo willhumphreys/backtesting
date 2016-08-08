137d4e632dd6e65d89ef44bf4ca1e0763766f375
Will Humphreys <will.humphreys@bbc.co.uk>
2016-08-08 12:05:50 +0100

    Write out the csv files to help with debugging.

--- RScripts/cumulativeProfit.r
+++ RScripts/cumulativeProfit.r
@@ -2,6 +2,7 @@ library(ggplot2)
 
 generate.plot <- function(file.in) {
     file.out <- paste("graphs/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], ".png", sep = "")
+    csv.out <- paste("r-csv/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], ".csv", sep = "")
     file.out.winLose <- paste("graphs/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], "winLose.png", sep = "")
 
     data <- read.table(paste("results/",file.in, sep=""), header=T,sep=",")
@@ -11,6 +12,8 @@ generate.plot <- function(file.in) {
     data$cumulative_profit <- cumsum(data$ticks)
     data$winLose <- ifelse(data$ticks > 0, 1,-1)
     data$cum.winLose <- cumsum(data$winLose)
+
+    write.table(data, file=csv.out, sep=",")
 
     ggplot(data=data, aes(x=date.time, y=cumulative_profit, group = 1)) +
     geom_line() +
