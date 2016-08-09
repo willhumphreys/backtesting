64eb99075300238fb58ef5e04f9f8b7821cf2b89
Will Humphreys <will.humphreys@bbc.co.uk>
2016-08-09 09:01:41 +0100

    Add moving average calculations.

--- RScripts/cumulativeProfit.r
+++ RScripts/cumulativeProfit.r
@@ -1,11 +1,15 @@
 library(ggplot2)
+library(TTR)
 
 
 last <- function(x) { tail(x, n = 1) }
 generate.plot <- function(file.in) {
+cat(file.in)
     file.out <- paste("graphs/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], ".png", sep = "")
     csv.out <- paste("r-csv/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], ".csv", sep = "")
     file.out.winLose <- paste("graphs/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], "winLose.png", sep = "")
+    file.out.sma30 <- paste("graphs/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], "sma30.png", sep = "")
+    file.out.sma30ticks <- paste("graphs/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], "sma30Ticks.png", sep = "")
 
     data <- read.table(paste("results/",file.in, sep=""), header=T,sep=",")
     data$date.time=as.POSIXct(data$date, tz = "UTC", format="%Y-%m-%dT%H:%M:%S")
@@ -25,13 +29,19 @@ generate.plot <- function(file.in) {
 
     data <- data[,c(7,1,2,3,4,5,6,8,9,10,11,12,13,14,15,16)]
 
+    data$SMA60 <- SMA(data$winLose,60)
+    data$SMA30 <- SMA(data$winLose,30)
+    data$SMA30Ticks <- SMA(data$ticks, 30)
+    data$SMA60Ticks <- SMA(data$ticks, 60)
+   # data$SMAYear <- SMA(data$cum.winLose,365)
+
     write.table(data, file=csv.out, sep=",", row.names=FALSE)
 
     data$date.time=as.Date(data$date.time)
 
     line <- paste(file.in, ",", last(data$cumulative_profit), ",", last(data$cum.winLose), ",", nrow(data))
     write(line,file="summary.csv",append=TRUE)
-
+cat(file.out)
     ggplot(data=data, aes(x=date.time, y=cumulative_profit, group = 1)) +
     geom_line() +
     scale_x_date(date_breaks = "3 month") +
@@ -39,7 +49,7 @@ generate.plot <- function(file.in) {
     stat_smooth() +
     ggtitle(file.in)
     ggsave(file=file.out)
-
+cat(file.out.winLose)
     ggplot(data=data, aes(x=date.time, y=cum.winLose)) +
     geom_line() +
     scale_x_date(date_breaks = "3 month") +
@@ -47,6 +57,27 @@ generate.plot <- function(file.in) {
     stat_smooth() +
     ggtitle(file.in)
     ggsave(file=file.out.winLose)
+
+cat(file.out.sma30)
+     ggplot(data=data, aes(x=date.time, y=SMA30)) +
+     geom_line() +
+     scale_x_date(date_breaks = "3 month") +
+     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+     stat_smooth() +
+     ggtitle(file.in)
+     ggsave(file=file.out.sma30)
+
+
+        cat(file.out.sma30ticks)
+     ggplot(data=data, aes(x=date.time, y=SMA30Ticks)) +
+     geom_line() +
+     scale_x_date(date_breaks = "3 month") +
+     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+     stat_smooth() +
+     ggtitle(file.in)
+     ggsave(file=file.out.sma30ticks)
+
+     cat('finished')
 }
 
 write("symbol,cumulative_profit,win_lose_count,trade_count", file="summary.csv",append=TRUE)
