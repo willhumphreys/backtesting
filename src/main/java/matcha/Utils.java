c26d3e2d96add22d1a8a6f9a3c6c0fe8f83e4ea2
Will Humphreys <will.humphreys@bbc.co.uk>
2016-09-12 17:27:42 +0100

    Don't be exact when calculating tick profit

--- src/main/java/matcha/Utils.java
+++ src/main/java/matcha/Utils.java
@@ -5,6 +5,6 @@ import java.math.BigDecimal;
 class Utils {
 
     int convertTicksToInt(BigDecimal doubleTicks, int decimalPointPlace) {
-        return doubleTicks.multiply(BigDecimal.valueOf(decimalPointPlace)).intValueExact();
+        return doubleTicks.multiply(BigDecimal.valueOf(decimalPointPlace)).intValue();
     }
 }
