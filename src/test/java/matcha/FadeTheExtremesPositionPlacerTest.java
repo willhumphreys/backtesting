4efab13b41e849396c6bb6feefe70e1df36650e8
Will Humphreys <will.humphreys@bbc.co.uk>
2016-09-12 15:15:44 +0100

    Add long test

--- src/test/java/matcha/FadeTheExtremesPositionPlacerTest.java
+++ src/test/java/matcha/FadeTheExtremesPositionPlacerTest.java
@@ -51,7 +51,43 @@ public class FadeTheExtremesPositionPlacerTest {
         assertThat(position.isPresent(), is(true));
         assertThat(position.get().getEntry(), is(equalTo(7.0)));
         assertThat(position.get().getEntryDate(), is(equalTo(LocalDateTime.parse("2007-12-13T18:19:00"))));
+        assertThat(position.get().getStop() > position.get().getTarget(), is(true));
         assertThat(position.get().getStop(), is(equalTo(9.0)));
         assertThat(position.get().getTarget(), is(equalTo(5.0)));
+    }
+
+    @Test
+    public void shouldCreateALongPosition() throws Exception {
+        final UsefulTickData usefulTickData = new UsefulTickDataBuilder()
+                .setCandleDate("2007-12-13T18:19:00")
+                .setCandleClose(7)
+                .setCandleLow(2)
+                .setPreviousCandleLow(0)
+                .setCandleHigh(9)
+                .setPreviousCandleHigh(0)
+                .setTakeOutYesterdaysLow(true)
+                .setClosePositive(true)
+                .setCloseAboveYesterdaysLow(true)
+                .setOpenAboveYesterdaysLow(true)
+                .setTakeOutYesterdaysHigh(false)
+                .setCloseNegative(false)
+                .setCloseBelowYesterdaysHigh(false)
+                .setOpenBelowYesterdaysHigh(false)
+                .setTodaysLow(0)
+                .setTodaysHigh(1)
+                .setLowOfDayForPreviousHour(4)
+                .setHighOfDayForPreviousHour(0.5)
+                .setTickLow(0)
+                .setTickHigh(0)
+                .createUsefulTickData();
+
+        final Optional<Position> position = fadeTheExtremesPositionPlacer.placePositions(usefulTickData, 5, 1);
+
+        assertThat(position.isPresent(), is(true));
+        assertThat(position.get().getStop() > position.get().getTarget(), is(false));
+        assertThat(position.get().getEntry(), is(equalTo(7.0)));
+        assertThat(position.get().getEntryDate(), is(equalTo(LocalDateTime.parse("2007-12-13T18:19:00"))));
+        assertThat(position.get().getStop(), is(equalTo(2.0)));
+        assertThat(position.get().getTarget(), is(equalTo(12.0)));
     }
 }
