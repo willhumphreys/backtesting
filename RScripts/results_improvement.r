b7da79326db5aa3aa4b8315ea5b91f73e3c4b7c9
Will Humphreys <will.humphreys@bbc.co.uk>
2016-10-04 13:07:25 +0100

    Add winner ratio, winning percentage and losing percentage

--- RScripts/results_improvement.r
+++ RScripts/results_improvement.r
@@ -10,6 +10,11 @@ output = args[2]
 
 data <- read.table(input, header=T,sep=",")
 
+data$winner_ratio <- round(data$winners / data$losers, digits = 4)
+
+data$winning_percentage <- round((data$winners / (data$winners + data$losers) * 100), digits = 2)
+data$losing_percentage <- round((data$losers / (data$winners + data$losers) * 100), digits = 2)
+
 cat(nrow(data))
 
 write.table(data, file=output, sep=",", row.names=FALSE)
