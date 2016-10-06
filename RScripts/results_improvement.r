60421be9882d8c01183910c1313f0eeec24ac44d
Will Humphreys <will.humphreys@bbc.co.uk>
2016-10-06 15:56:10 +0100

    Start working on the results improvement

--- RScripts/results_improvement.r
+++ RScripts/results_improvement.r
@@ -1,4 +1,5 @@
 library(R.utils)
+library(plyr)
 
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -10,7 +11,7 @@ output = args[2]
 
 cat("Executing results_improvement.r\n")
 
-data <- read.table(input, header=T,sep=",")
+data <- read.table(file.path(input, 'results.csv'), header=T,sep=",")
 
 data$winner_ratio <- round(data$winners / data$losers, digits = 4)
 
@@ -19,6 +20,40 @@ data$losing_percentage <- round((data$losers / (data$winners + data$losers) * 10
 
 cat(nrow(data))
 
+
+combined_symbol_data <- data.frame(symbol = character(0), tradeCount= integer(0))
+
+generate.symbol.info <- function(file.in) {
+
+  symbol <- strsplit(file.in, split='.', fixed=TRUE)[[1]][1]
+  symbol_data <- read.table(file.path(input, 'data', file.in), header=T,sep=",")
+  symbol_data$symbol <- rep(symbol,nrow(symbol_data))
+  return(symbol_data)
+
+}
+
+input.files <- list.files(file.path(input, 'data'))
+
+with_symbols <- lapply(input.files, function(x) generate.symbol.info(x))
+
+symbols_merged <- do.call(rbind, with_symbols)
+symbols_merged$date=as.POSIXct(symbols_merged$date, tz = "UTC", format="%Y-%m-%dT%H:%M")
+symbols_merged$years <- format(as.Date(symbols_merged$date, format="%d/%m/%Y"),"%Y")
+symbols_merged$winner <- symbols_merged$ticks > 0
+years <- unique(symbols_merged$years)
+
+print(xtabs(~ symbol + years, symbols_merged))
+
+winners_by_year <- (count(symbols_merged, c('symbol', 'years', 'winner')))
+
+print(head(winners_by_year))
+#print(tail(symbols_merged))
+
+print(head(data))
+
+print(head(merge(data, winners_by_year,by="symbol")))
+
 write.table(data, file=output, sep=",", row.names=FALSE)
 
 cat("Finished executing cumulative_profit.r\n")
+
--- results_improvement.sh
+++ results_improvement.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-Rscript RScripts/results_improvement.r /Users/humphw03/backtesting/results/results_bands/results.csv /Users/humphw03/backtesting/results/results_bands/results_improved.csv
+Rscript RScripts/results_improvement.r /Users/humphw03/backtesting/results/results_bands /Users/humphw03/backtesting/results/results_bands/results_improved.csv
