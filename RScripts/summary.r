5b9e21557de125b6abb337310b53a9b5a995b64f
Will Humphreys <will.humphreys@bbc.co.uk>
2016-10-06 11:20:56 +0100

    Start adding information for each symbol

--- RScripts/summary.r
+++ RScripts/summary.r
@@ -22,13 +22,19 @@ flat <- sum(data$winner_ratio == 1)
 highest_winners_row <- data[which.max(data$winning_percentage),]
 lowest_winners_row <- data[which.min(data$winning_percentage),]
 
-fileConn<-file(output)
+fileConn<-file(output, "wt")
 writeLines(c(sprintf("Winners: %d\nLosers: %d\n", winners, losers),
     sprintf("Highest winning percentage: %s:%f", highest_winners_row$symbol, highest_winners_row$winning_percentage),
     sprintf("Lowest winning percentage: %s:%f", lowest_winners_row$symbol, lowest_winners_row$winning_percentage)),
-    con=fileConn)
-close(fileConn)
+    con=fileConn,sep="\n")
 
+symbol <- 'AUDUSD'
+symbol_data <- read.table(file.path(input, paste("data/", symbol, ".csv", sep="")), header=T,sep=",")
+trade_count <- nrow(symbol_data)
+cat("\nSymbol information\n", file=fileConn, append=TRUE)
+cat(sprintf("%s trade count %d\n", symbol, trade_count), file=fileConn, append=TRUE)
+
+close(fileConn)
 print('Finished summary.r')
 
 
