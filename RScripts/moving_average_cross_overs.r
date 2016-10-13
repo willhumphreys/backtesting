f192906d07e6c69f625056aecac5ec1155f69937
Will Humphreys <will.humphreys@bbc.co.uk>
2016-10-13 19:53:28 +0100

    Add moving average crossovers r script

--- RScripts/moving_average_cross_overs.r
+++ RScripts/moving_average_cross_overs.r
@@ -13,10 +13,12 @@ output <- args[2]
 
 generate.all.crossovers <- function(long_sma, short_sma) {
 
-  print(sprintf('Generate graphs and data for %d', moving_average_length))
+  print(sprintf('Generate crossover graphs and data for %d %d', long_sma, short_sma))
 
-  graph.output <- file.path(output, 'graphs/bollingers', moving_average_length)
-  data.output <- file.path(output, 'data_bollingers', moving_average_length)
+  crossovers <- paste(long_sma, short_sma, sep="_")
+
+  graph.output <- file.path(output, 'graphs/crossovers', crossovers)
+  data.output <- file.path(output, 'data_crossovers', crossovers)
 
   dir.create(graph.output, recursive = TRUE)
   dir.create(data.output, recursive = TRUE)
@@ -32,29 +34,21 @@ generate.all.crossovers <- function(long_sma, short_sma) {
 
     data$winLose <- ifelse(data$ticks > 0, 1, -1)
 
-    data$sma <- SMA(data$winLose,n=moving_average_length)
-
-    bands <- data.frame(BBands( data[,c("sma")], n = 70 , sd=2.0))
-
-    data$downBB <- bands$dn
-    data$upBB <- bands$up
-    data$mavg <- bands$mavg
+    data$long.crossover <- SMA(data$winLose,n=long_sma)
+    data$short.crossover <- SMA(data$winLose,n=short_sma)
 
     ggplot(data=data, aes(date)) +
-    geom_line(aes(y=sma)) +
-    geom_line(aes(y=downBB)) +
-    geom_line(aes(y=upBB)) +
-    geom_line(aes(y=mavg)) +
-    geom_point(aes(y=sma)) +
+    geom_line(aes(y=long.crossover, colour="darkred")) +
+    geom_line(aes(y=short.crossover, colour="green3")) +
     geom_hline(yintercept = 0) +
     geom_hline(yintercept = -0.5, colour="#990000", linetype="dashed") +
     geom_hline(yintercept = 0.5, colour="#990000", linetype="dashed") +
     scale_y_continuous(breaks=seq(-1,1,0.05)) +
     geom_hline(yintercept = mean(data$sma10, na.rm=TRUE), colour="royalblue1", linetype="dashed") +
-    ggtitle(paste(moving_average_length, ' day sma', sep=""))
+    ggtitle(paste(crossovers, ' crossover', sep=""))
     ggsave(file=file.path(graph.output, paste(symbol, '.png', sep="")))
 
-    write.table(data, file=file.path(data.output, paste(symbol, '-bollingers.csv', sep="")), sep=",", row.names=FALSE)
+    write.table(data, file=file.path(data.output, paste(symbol, '-crossovers.csv', sep="")), sep=",", row.names=FALSE)
 
   }
   sapply(input.files, function(x) generate.bollinger.data(x))
@@ -62,6 +56,6 @@ generate.all.crossovers <- function(long_sma, short_sma) {
 
 moving_average_lengths <- seq(10,100, 10)
 moving_average_combinations <- expand.grid(long.sma = moving_average_lengths,short.sma = moving_average_lengths)
-moving_average_combinations[moving_average_combinations$long.sma > moving_average_combinations$short.sma, ]
+moving_average_combinations <- moving_average_combinations[moving_average_combinations$long.sma > moving_average_combinations$short.sma, ]
 
 apply(moving_average_combinations, 1, function(x) generate.all.crossovers(x[1], x[2]))
---
+++ executeMovingAverageCrossovers.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+Rscript RScripts/moving_average_cross_overs.r results/normal results/normal
